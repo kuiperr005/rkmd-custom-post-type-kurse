fb50e6de321cbedf2ab5fb7d110dcc11fbb911d6
RK Mediadesign <info@rkmediadesign.nl>
2019-01-27 16:15:37 +0100

    Setup custom post type

---
+++ includes/class-rkmd-custom-post-type-kurse-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://rkmediadesign.nl
+ * @since      1.0.0
+ *
+ * @package    RKMD_Custom_Post_Type_Kurse
+ * @subpackage RKMD_Custom_Post_Type_Kurse/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    RKMD_Custom_Post_Type_Kurse
+ * @subpackage RKMD_Custom_Post_Type_Kurse/includes
+ * @author     RK Mediadesign <info@rkmediadesign.nl>
+ */
+class RKMD_Custom_Post_Type_Kurse_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
